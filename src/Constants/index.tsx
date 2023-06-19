933fc5f04960949cf2fe6e08f14b76344b302a89
Saleem Castle <saleemcastle94@gmail.com>
2023-06-18 23:25:37 -0500

    begin sidebar implementation

---
+++ src/Constants/index.tsx
@@ -0,0 +1,16 @@
+export const sidebarTabs = [
+    'Overview',
+    'Calendar',
+    'Patient List',
+    'Messages',
+    'Payment Information',
+    'Settings'
+]
+export enum Tabs {
+    Overview ='Overview',
+    Calendar ='Calendar',
+    PatientList ='Patient List',
+    Messages ='Messages',
+    PaymentInformation ='Payment Information',
+    Settings ='Settings'
+}
